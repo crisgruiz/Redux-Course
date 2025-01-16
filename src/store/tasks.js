0588ad545d46ddd4a930879e15f4b21d3f8e1c4f
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-01-16 15:11:09 +0100

    refactor: use createSlice

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,33 +1,66 @@
- import { createAction, createReducer } from "@reduxjs/toolkit"
-
-// Actions
-export const addTask = createAction("ADD_TASK")
-export const removeTask = createAction("REMOVE_TASK")
-export const completeTask = createAction("COMPLETE_TASK")
-
-// Reducer
+ import { createSlice } from "@reduxjs/toolkit"
 
 let id = 0
 
-const reducer = createReducer([], (builder) => {
-    builder
-      .addCase(addTask, (state, action) => {
+ const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: [],
+    reducers: {
+      addTask: (state, action) => {
         state.push({
           id: ++id, 
           task: action.payload.task, 
           complete: false
         })
-      })
-      .addCase(removeTask, (state, action) => { 
-        const index = state.findIndex(task => task.id === action.payload.id) 
-        state.splice(index, 1)
-      })
-      .addCase(completeTask, (state, action) => {
+      },
+      removeTask: (state, action) => {
+        const index = state.findIndex(task => task.id === action.payload.id)
+        if (index !== -1) {
+          state.splice(index, 1)
+        }
+      },
+      completeTask: (state, action) => {
         const index = state.findIndex(task => task.id === action.payload.id)
         if (index !== -1) {
           state[index].complete = true
         }
-      })
+      }
+    }
   })
+
+  export const { addTask, removeTask, completeTask } = tasksSlice.actions
+  export default tasksSlice.reducer
+
+// CreateReducer method
+
+// // Actions
+// export const addTask = createAction("ADD_TASK")
+// export const removeTask = createAction("REMOVE_TASK")
+// export const completeTask = createAction("COMPLETE_TASK")
+
+// // Reducer
+
+// let id = 0
+
+// const reducer = createReducer([], (builder) => {
+//     builder
+//       .addCase(addTask, (state, action) => {
+//         state.push({
+//           id: ++id, 
+//           task: action.payload.task, 
+//           complete: false
+//         })
+//       })
+//       .addCase(removeTask, (state, action) => { 
+//         const index = state.findIndex(task => task.id === action.payload.id) 
+//         state.splice(index, 1)
+//       })
+//       .addCase(completeTask, (state, action) => {
+//         const index = state.findIndex(task => task.id === action.payload.id)
+//         if (index !== -1) {
+//           state[index].complete = true
+//         }
+//       })
+//   })
   
-  export default reducer
+//   export default reducer
