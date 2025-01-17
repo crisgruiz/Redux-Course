13929ef731aa47dd9c4a72d24099503e6eb0516c
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-01-17 14:37:29 +0100

    feat: create middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import employeesReducer from "./employees";
 import taskReducer from "./tasks"
 import { configureStore } from "@reduxjs/toolkit";
+import log from "../middleware/log";
 
 const store = configureStore({reducer: {
     tasks: taskReducer,
-    employees: employeesReducer,
-
-}})
+    employees: employeesReducer},
+middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), log]
+})
 
 export default store
