5a2cc3c62c1d9c9d6cd45dbbf611efc71dac8507
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-01-17 14:52:52 +0100

    feat: install and use redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,12 @@
 import employeesReducer from "./employees";
 import taskReducer from "./tasks"
-import { configureStore } from "@reduxjs/toolkit";
-import log from "../middleware/log";
+import { configureStore } from "@reduxjs/toolkit"
+import logger from "redux-logger";
 
 const store = configureStore({reducer: {
     tasks: taskReducer,
     employees: employeesReducer},
-middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), log]
+middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger]
 })
 
 export default store
