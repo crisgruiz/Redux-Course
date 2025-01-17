6981a5d93c86139fd65b755ec5c9c4d4a56bb57e
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-01-17 17:14:50 +0100

    feat: add error scenario and clean file

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import error from "./middleware/error";
+
 let id = 0;
 
 const initialState = {
@@ -9,10 +9,17 @@ const initialState = {
   error: null,
 };
 
-export const fetchTasks = createAsyncThunk("fetchTasks", async () => {
-  const response = await axios.get("http://localhost:5500/api/tasks");
-  return { tasks: response.data };
-});
+export const fetchTasks = createAsyncThunk(
+  "fetchTasks",
+  async (a, { rejectWithValue }) => {
+    try {
+      const response = await axios.get("http://localhost:5500/api/tasks");
+      return { tasks: response.data };
+    } catch (error) {
+      return rejectWithValue({ error: error.message });
+    }
+  }
+);
 
 const tasksSlice = createSlice({
   name: "tasks",
@@ -53,43 +60,13 @@ const tasksSlice = createSlice({
       state.tasks = action.payload.tasks;
       state.loading = false;
     });
+    builder.addCase(fetchTasks.rejected, (state, action) => {
+      state.error = action.payload.error;
+      state.loading = false;
+    });
   },
 });
 
 export const { getTasks, addTask, removeTask, completeTask } =
   tasksSlice.actions;
 export default tasksSlice.reducer;
-
-// CreateReducer method
-
-// // Actions
-// export const addTask = createAction("ADD_TASK")
-// export const removeTask = createAction("REMOVE_TASK")
-// export const completeTask = createAction("COMPLETE_TASK")
-
-// // Reducer
-
-// let id = 0
-
-// const reducer = createReducer([], (builder) => {
-//     builder
-//       .addCase(addTask, (state, action) => {
-//         state.push({
-//           id: ++id,
-//           task: action.payload.task,
-//           complete: false
-//         })
-//       })
-//       .addCase(removeTask, (state, action) => {
-//         const index = state.findIndex(task => task.id === action.payload.id)
-//         state.splice(index, 1)
-//       })
-//       .addCase(completeTask, (state, action) => {
-//         const index = state.findIndex(task => task.id === action.payload.id)
-//         if (index !== -1) {
-//           state[index].complete = true
-//         }
-//       })
-//   })
-
-//   export default reducer
