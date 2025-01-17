b5061a2248bd49d80d3599b9449e2c830eaa5670
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-01-17 14:54:54 +0100

    feat(second exercise): create and use error middleware

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import { addEmployee } from "./store/employees";
 import store from "./store/configureStore";
 
 store.dispatch(addEmployee({name: "Cristina"}))
+
+//Error example
+store.dispatch({type: "SHOW_ERROR", payload: {error: "User not found"} })
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,12 @@ import employeesReducer from "./employees";
 import taskReducer from "./tasks"
 import { configureStore } from "@reduxjs/toolkit"
 import logger from "redux-logger";
+import error from "../middleware/error";
 
 const store = configureStore({reducer: {
     tasks: taskReducer,
     employees: employeesReducer},
-middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger]
+middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger, error]
 })
 
 export default store
